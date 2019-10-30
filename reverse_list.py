1b66563aca0cf0aeacb21434cb14eaa9be03b185
Ralphie Perez <ironware6@gmail.com>
2019-10-30 14:17:01 -0400

    First and ONLY commit -RP

---
+++ reverse_list.py
@@ -0,0 +1,13 @@
+num = [3,2,4,1,0,2,3,4,5,5]
+
+
+def reverse_list(arr):
+  new_list = []
+  for n in range (len(arr)):
+    reverb = arr[-1 + (-1 * n)]
+    new_list.append(reverb)
+
+  print(new_list)  
+
+
+reverse_list(num)
